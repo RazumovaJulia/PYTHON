9262485b791d427458a20eb9cf201ec2246aea7d
julia.razumova <julia.razumova@hys-enterprise.com>
2019-10-28 15:04:11 +0200

    added files

---
+++ Test.py
@@ -0,0 +1,21 @@
+var_int = 10
+var_float = 8.4
+var_str = "No"
+
+big_int = var_int*3.5
+print (big_int)
+
+var_float = 8.4 - int('1')
+print(var_float)
+
+print(var_int/var_float)
+print(big_int/var_float)
+
+var_str = var_str*2 + "Yes"*3
+print(var_str)
+
+
+
+
+
+
