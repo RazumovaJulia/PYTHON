76eca710a82bf5f5bab75d4687bf0073138ff557
julia.razumova <julia.razumova@hys-enterprise.com>
2019-11-01 17:44:43 +0200

    added files

--- Homework2.py
+++ Homework2.py
@@ -1,30 +1,24 @@
-# 1. Преобразовать строку "ecnalubma" в её зеркальное отражение(реверсировать). Сделать это двумя способами.
+#1. Преобразовать строку "ecnalubma" в её зеркальное отражение(реверсировать). Сделать это двумя способами.
 # input: 'ecnalubma'
 # output: 'ambulance'
-# text = input("ecnalubma")
-# print(text[::-1])
+text = "ecnalubma"
+print(text[::-1])
 
-# text2 = 'ecnalubma'
-# result = []
-# resultStr = ''
-# for i in range(8, -1, -1):
-#     result.append(text2[i])
+text2 = 'ecnalubma'
+result = list(text2)
+result.reverse()
+print(''.join(result))
 #
-# print(resultStr.join(result))
-# print(result)
+stringResult = 'one two three four'
+stringResult2 = []
+stringList = stringResult.split()
+stringList.reverse()
+print(stringList)
 
-
-# 2.
-# stringResult = 'one two three four'
-# stringResult2 = []
-# stringList = stringResult.split()
-# stringList.reverse()
+for i in stringList[::-1]:
+    stringResult2.append(i)
+print(stringResult2)
 #
-# print(stringList)
-# for i in range(len(stringList)-1, -1, -1):
-#     stringResult2.append(stringList[i])
-#
-# print(stringResult2)
 
 
 items = [10, -15, 3, 8, 0, 9, -6, 13, -1, 5]
@@ -33,10 +27,9 @@ for i in items:
     if i > 0:
         changedItem.append(1)
     elif i < 0:
-        changedItem.append(-1)
-    else:
-        changedItem.append(0)
-
+     changedItem.append(-1)
+else:
+ changedItem.append(0)
 print(items)
 print(changedItem)
 
