5b848c43163c3571a1522b71932dd04601ce5877
julia.razumova <julia.razumova@hys-enterprise.com>
2019-11-08 15:25:03 +0200

    added file

--- while.py
+++ while.py
@@ -2,23 +2,42 @@
 # substring = 'wow'
 # print(string.count(substring))
 
-def count(string,substring):
-   result = 0
-   i = 0
-   while i > -1:
-       try:
-           i = string.index(substring, i) + len(substring)
-           result += 1
-           i -= 1
-       except:
-           i = -1
+# def count(string,substring):
+#    result = 0
+#    i = 0
+#    while i > -1:
+#        try:
+#            i = string.index(substring, i) + len(substring)
+#            result += 1
+#            i -= 1
+#        except:
+#            i = -1
+#
+#    return result
+#
+#
+# string = "wowhatamanwowowpalehche"
+# substring = "wow"
+# print (count(string,substring))
 
-   return result
+def count(string, substring):
+    result = 0
+    i =0
+    while i < len(string):
+        if string[i:i+len(substring)] == substring:
+            result += 1
+        i += 1
+
+    return result
+
 
 
 string = "wowhatamanwowowpalehche"
 substring = "wow"
 print (count(string,substring))
+
+
+
 
 
 
